0eb0b0f027ff05b2b76206c596c2aa3e57944e2a
Bittu Kumar <bittubaudh5@gmail.com>
2023-05-03 00:39:27 +0530

    another new  solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {
             return x*y;
         }
@@ -12,13 +12,10 @@ public class Main {
             return x*y;
         }
     }
-    public static void Main(String[] args){
+    public static void main(String[] args){
         Product p = new Product();
-//        System.out.println(p.product(5, 5));
-//        System.out.println(p.product(5, 2, 3));
-//        System.out.println(p.product(2.5, 6.8));
         p.product(5, 5);
         p.product(5, 2, 3);
-        p.product(2.5, 6.8);
+        p.product(2.545, 6.058);
     }
 }
